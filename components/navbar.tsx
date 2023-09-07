44d15ea10f7f68342e72d3416a981af904dc6b14
dbenavi5 <56004267+dbenavi5@users.noreply.github.com>
2023-09-07 12:21:16 -0700

    create navbar and integrate dark mode toggle

---
+++ components/navbar.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import Link from "next/link";
+import { UserButton } from "@clerk/nextjs";
+import { Poppins } from "next/font/google";
+import { Button } from "@/components/ui/button";
+import { Sparkles } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { ModeToggle } from "@/components/mode-toggle";
+
+const poppins = Poppins({ weight: "600", subsets: ["latin"] });
+
+interface NavbarProps {
+  isPro: boolean;
+}
+
+export const Navbar = () => {
+  return (
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 h-16 border-b border-primary/10 bg-secondary">
+      <div className="flex items-center">
+        {/* <MobileSidebar/> */}
+        <Link href="/">
+          <h1
+            className={cn(
+              "hidden md:block text-xl md:text-3xl font-bold text-primary",
+              poppins.className
+            )}
+          >
+            companion.ai
+          </h1>
+        </Link>
+      </div>
+      <div className="flex items-center gap-x-3">
+        <Button size="sm" variant="premium">
+          Upgrade
+          <Sparkles className="h-4 w-4 fill-white ml-2" />
+        </Button>
+        <ModeToggle/>
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </div>
+  );
+};
