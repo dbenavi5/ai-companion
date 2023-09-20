e61832f7fc4984bb382d6acad56ce57765caa177
dbenavi5 <56004267+dbenavi5@users.noreply.github.com>
2023-09-20 09:56:28 -0700

    create memory system&instal pinecone&upstash/redis

--- components/chat-message.tsx
+++ components/chat-message.tsx
@@ -47,7 +47,7 @@ export const ChatMessage = ({
       )}
     >
       {role !== "user" && src && <BotAvatar src={src} />}
-      <div className="rounded-md px-4 py-2 max-w-xl text-sm bg-primary/10">
+      <div className="rounded-md px-4 py-2 max-w-sm text-sm bg-primary/10">
         {isLoading ? (
           <BeatLoader color={theme === "light" ? "black" : "white"} size={5} />
         ) : role !== "user" ? (
