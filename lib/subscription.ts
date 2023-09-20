acf7f469a7412cac94c36b8227bf266d5800ea00
dbenavi5 <56004267+dbenavi5@users.noreply.github.com>
2023-09-20 13:51:50 -0700

    create stripe & webhook api for user subscription

---
+++ lib/subscription.ts
@@ -0,0 +1,34 @@
+import { auth } from "@clerk/nextjs";
+import prismadb from "@/lib/prismadb";
+
+const DAYS_IN_MS = 86_400_000;
+
+export const checkSubscription = async () => {
+    const { userId } = auth();
+
+    if (!userId) {
+        return false;
+    }
+
+    const userSubscription = await prismadb.userSubscription.findUnique({
+        where: {
+            userId,
+        },
+        select: {
+            stripeSubscriptionId: true,
+            stripeCustomerId: true,
+            stripePriceId: true,
+            stripeCurrentPeriodEnd: true,
+        }
+    });
+
+    if (!userSubscription) {
+        return false;
+    }
+
+    const isValid =
+        userSubscription.stripePriceId &&
+        userSubscription.stripeCurrentPeriodEnd?.getTime()! + DAYS_IN_MS > Date.now();
+
+    return !!isValid;
+}
